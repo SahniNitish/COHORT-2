2efb7d139648890efc5731bfeb4ec9d61a1e878e
Nitish sahni <nitish@realmeta.ca>
2025-12-27 12:31:46 -0400

    first commit

---
+++ PRACTICE/Class1.js
@@ -0,0 +1,22 @@
+//Simple Primitives
+/* Variables (let , Var , Const)
+   Data Types - String , Number and Booleans
+   if/else
+   Loops - for Loop 
+*/
+
+//Write a the program to greet a person given their first and last name 
+
+let firstName = "Nitish ";
+let lastName  = "Sahni";
+
+console.log("Hello Mr " + firstName + lastName + " to our course");
+
+//
+let answer = 0 ;
+for(i = 0 ; i<10; i++){
+    answer = answer+1;
+    console.log(i);
+}
+
+console.log(answer);
---
+++ PRACTICE/Class2.js
@@ -0,0 +1,11 @@
+//Complex Primitives
+
+//write a program to print all the even numbers in a array
+const ages = [21 , 22 , 33 , 44 , 33 , 31 , 77, 99];
+
+for (i = 0 ; i<ages.length; i++){
+
+    if (ages[i] % 2 == 0 ){
+        console.log(ages[i]);
+    }
+}
