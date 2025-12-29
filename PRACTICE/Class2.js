5453ac9521bfb9b1bfc1185b577900ae01ade046
Nitish sahni <nitish@realmeta.ca>
2025-12-29 10:53:41 -0400

    Arrow fucntion

--- PRACTICE/Class2.js
+++ PRACTICE/Class2.js
@@ -1,11 +1,33 @@
 //Complex Primitives
 
 //write a program to print all the even numbers in a array
-const ages = [21 , 22 , 33 , 44 , 33 , 31 , 77, 99];
+// const ages = [21 , 22 , 33 , 44 , 33 , 31 , 77, 99];
 
-for (i = 0 ; i<ages.length; i++){
+// for (i = 0 ; i<ages.length; i++){
 
-    if (ages[i] % 2 == 0 ){
-        console.log(ages[i]);
+//     if (ages[i] % 2 == 0 ){
+//         console.log(ages[i]);
+//     }
+// }
+
+/*
+write a program that print all the male people's first name 
+given a complex object
+ */
+
+ const allUsers = [{
+    firstName:"Nitish",
+    gender   :"male"
+    },
+    {
+    firstName:"kashish",
+    gender   :"Female"
+}]
+
+for (let  i = 0 ; i<allUsers.length; i++){
+    if(allUsers[i].gender == "male"){
+        console.log(allUsers[i].firstName)
     }
 }
+
+//functions
---
+++ PRACTICE/class3.js
@@ -0,0 +1,12 @@
+//Arrow fucntion 
+
+const greet = (count)=>{
+    for(i=0 ; i<count; i++){
+    console.log("Hello World")
+}
+}
+greet(3);
+
+const square = (num) => num*num;
+
+console.log(square(3));
