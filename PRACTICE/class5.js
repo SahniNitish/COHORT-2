2f799014cbc7d2a1d6bd589adb1a68e6a4da06c3
Nitish sahni <nitish@realmeta.ca>
2025-12-29 12:27:58 -0400

    CallBack Hell

---
+++ PRACTICE/class5.js
@@ -0,0 +1,50 @@
+// //setTimeOut()
+
+// console.log("hello");
+
+// setTimeout( ()=>{
+//     console.log("This Line will print after 3 sec.")
+// } , 3000);
+
+// console.log("Hello world - 2");
+// console.log("Hello   World3 ") 
+
+function getCheese(callback){
+   setTimeout(()=>{
+    const cheese = "CHEEEESSEE";
+    console.log("Here is the CHeeese" , cheese);
+    callback(cheese);
+   },2000);  
+} 
+
+function makeDough(cheese , callback){
+    setTimeout(()=>{
+        const Dough = "Pizza Base";
+        console.log("Here is the pizza Base ready!!!" , Dough);
+        callback(Dough);
+    } , 3000);
+}
+
+function Baking(Dough , callback){
+    setTimeout(()=>{
+        const Pizza = "Pizza Ready";
+        console.log("Pizza is baked" , Pizza);
+        callback(Pizza)
+    },4000);
+}
+
+
+
+
+
+
+getCheese((cheese)=>{
+    makeDough(cheese ,(Dough)=>{
+        Baking(Dough  ,(Pizza)=>{
+        console.log("Pizza is ready to eat " , Pizza);
+        });
+    });
+   });
+
+
+// pizza - > dough -> cheese
